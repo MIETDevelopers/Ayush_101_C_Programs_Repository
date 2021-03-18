a2bf916af013e6cd560c3436987680dc7d40623c
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-18 11:15:55 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
-int main(){
-	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
-	scanf("%f %f %f %f", &raduisOfCircle, &sideOfSquare, &legthOfRectangle, &breadthOfRectangle);
-	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
-	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
-	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
+// Author: Ayush Bhat
+// Creation Date: 17/3/21
+// Purpose Of The Program:To find the area and perimeter of circle, square and rectangle based on user choice
+#include<stdio.h>  // preprocessor directive to include header file having standard input and output functions
+int main(){  // main function body
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;  //declaration of variables of float data type
+	scanf("%f %f %f %f", &raduisOfCircle, &sideOfSquare, &legthOfRectangle, &breadthOfRectangle);  //taking input from user
+	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));  //printing Area of circle
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));  //printing area of square
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));  //area of rectangle
 	return 0;
 }
