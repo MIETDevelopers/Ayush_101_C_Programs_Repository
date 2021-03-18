cd7f77d801e645ab1215790af92bce01277ad073
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-18 11:50:48 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -4,7 +4,7 @@
 #include<stdio.h>   // preprocessor directive to include "stdio.h" header file having standard input and output functions
 int main() {  //main function body
    int num1, num2, sum;   //variable declaration of integer data type
-   printf("\nEnter two no: ");  //printing two numbers given by user
+   printf("\nEnter two no: ");  //printing Enter two no:
    scanf("%d %d", &num1, &num2);  //taking two numbers bfrom user
    sum = num1 + num2;  //sum of two numbers
    printf("Sum : %d", sum);  //printing the sum of two numbers
