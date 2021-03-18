d59ccedbba8fa515139d2b8e5c222b582bd46121
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-18 12:23:02 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,12 +1,15 @@
-#include<stdio.h>  
-int main()    
+// Author: Ayush Bhat
+// Creation Date: 17/3/21
+// Purpose Of The Program: to find the factorial using loop
+#include<stdio.h>  // preprocessor directive to include "stdio.h" header file having standard input and output functions
+int main()    //main function body
 {    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
+ int i,fact=1,number;    //variable declaration of integer data type
+ printf("Enter a number: ");    //print Enter a number
+  scanf("%d",&number);    //taking a number from user for finding factorial
+    for(i=1;i<=number;i++){    //using for loop to find factorial
       fact=fact*i;    
   }    
-  printf("Factorial of %d is: %d",number,fact);    
+  printf("Factorial of %d is: %d",number,fact);    //print the value of Factorial of number
 return 0;  
 }   
