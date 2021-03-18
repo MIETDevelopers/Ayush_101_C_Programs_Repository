1661cec06a98074ed44bc50f277bb506012d686b
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-18 11:39:46 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
-int main() {
-   int num1, num2, sum;
-   printf("\nEnter two no: ");
-   scanf("%d %d", &num1, &num2);
-   sum = num1 + num2;
-   printf("Sum : %d", sum);
+// Author: Ayush Bhat
+// Creation Date: 16/3/21
+// Purpose Of The Program: to find the sum of two numbers 
+#include<stdio.h>   // preprocessor directive to include "stdio.h" header file having standard input and output functions
+int main() {  //main function body
+   int num1, num2, sum;   //variable declaration of integer data type
+   printf("\nEnter two no: ");  //printing two numbers given by user
+   scanf("%d %d", &num1, &num2);  //taking two numbers bfrom user
+   sum = num1 + num2;  //sum of two numbers
+   printf("Sum : %d", sum);  //printing the sum of two numbers
    return(0);
 }
