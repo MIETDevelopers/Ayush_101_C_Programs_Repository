6b276393604d8389d8f3d6d64bcf8384b545d54a
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-18 10:31:08 +0530

    Update Euclidean_distance.c

--- Euclidean_distance.c
+++ Euclidean_distance.c
@@ -1,19 +1,24 @@
-#include<stdio.h>  
-#include<math.h>  
+// Author: Ayush Bhat
+// Creation Date: 17/3/21
+// Purpose Of The Program:To find the Eucledian distance between two points
+#include<stdio.h>  // preprocessor directive to include header file having standard input and output functions
+#include<math.h>  // preprocessor directive to include header file having math functions
   
-int main()  
+int main()  // main function body
 {  
-    float x1, y1, x2, y2, distance;  
-  
+    float x1, y1, x2, y2, distance;  //variable declaration of float data type
+ 
+  //taking first point's coordinate
     printf("Enter point 1 (x1, y1)\n");  
     scanf("%f%f", &x1, &y1);  
   
+  //taking second point's coordinate
     printf("Enter point 2 (x2, y2)\n");  
     scanf("%f%f", &x2, &y2);  
   
-    distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) );  
+    distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) );  //calculating distance
   
-    printf("Distance between (%0.2f, %0.2f) and (%0.2f, %0.2f) is %0.2f\n", x1, y1, x2, y2, distance);  
+    printf("Distance between (%0.2f, %0.2f) and (%0.2f, %0.2f) is %0.2f\n", x1, y1, x2, y2, distance);  //display result
   
     return 0;  
 }  
