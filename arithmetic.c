2eac2b5187aee1fc668f7da871156f8422ab8041
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-18 11:29:52 +0530

    Update arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
+// Author: Ayush Bhat
+// Creation Date: 16/3/21
+// Purpose Of The Program: Arithmetic operations using addition, subtraction, multiplication and remainder operator
+#include <stdio.h>  // preprocessor directive to include header file having standard input and output functions
  
-int main()
+int main()  // main function body
 {
-    int num1, num2;
+ // declaration of variables of int data type
+    int num1, num2;  
     int sum, sub, mult, div;
-    printf("Enter two numbers: \n");
  
+    //print two numbers given by user
+    printf("Enter two numbers: \n"); 
     scanf("%d %d", &num1, &num2);
+ 
     sum = num1 + num2;   //Sum of two numbers
-    printf("num1+num2 = %d\n", sum);
+    printf("num1+num2 = %d\n", sum); //print sum of two numbers
  
     sub = num1 - num2;   //Subtraction of two numbers
-  
-    printf("num1 - num2 = %d\n", sub);
+    printf("num1 - num2 = %d\n", sub); //print subtraction of two numbers
  
     mult = num1 * num2;   //Multiplication of twi numbers
-   
-    printf("num1 * num2 = %d\n", mult);
+    printf("num1 * num2 = %d\n", mult);  //print multiplication of two numbers
  
-    div = num1 / num2;   //Division of two numbers
-
-    printf("num1 / num2 = %d\n", div);
+    div = num1 / num2;   //Division of two numbers 
+    printf("num1 / num2 = %d\n", div);  //print division of two numbers
     return 0;
 }
