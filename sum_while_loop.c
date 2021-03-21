d91303a1886d6a29fa98aba7f109e23a633b958f
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-22 01:01:13 +0530

    sum_while_loop

---
+++ sum_while_loop.c
@@ -0,0 +1,23 @@
+// Author: Ayush Bhat
+// Creation Date: 18/3/21
+// Purpose Of The Program: Calculate sum of digits of a number using while loop
+#include<stdio.h>  // preprocessor directive to include "stdio.h" header file having standard input and output functions
+int main()  // main function body
+{  
+  int Number, Remainder, Sum=0;  //variable declaration, intialization of sum
+
+  printf("Please Enter any number\n");  //print Please Enter any number
+  scanf("%d", &Number);  //takes number from user
+
+  while(Number > 0)//Using while loop.
+  {
+  	//This is the formula to calculate sum of digits
+     Remainder = Number % 10;
+     Sum = Sum+Reminder;
+     Number = Number / 10;
+  }
+
+  printf("Sum of the digits of Given Number = %d\n", Sum);  //print Sum of two digits of Given Number = 
+
+  return 0;
+}
