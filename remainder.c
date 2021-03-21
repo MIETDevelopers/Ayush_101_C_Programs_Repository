4285b35e6f0239b968a08b64e439335cc4656493
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-22 00:16:33 +0530

    Update remainder.c

--- remainder.c
+++ remainder.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Author: Ayush Bhat
 // Creation Date: 17/3/21
 // Purpose Of The Program: Arithmetic operation using remainder operator
@@ -17,24 +16,4 @@ int main() {  //main function body
     printf("Quotient = %d\n", quotient);  //print Quotient
     printf("Remainder = %d", remainder);  //print Remainder
     return 0;
-=======
-// Author: Ayush Bhat
-// Creation Date: 17/3/21
-// Purpose Of The Program: Arithmetic operation using remainder operator
-#include <stdio.h>  // preprocessor directive to include "stdio.h" header file having standard input and output functions
-int main() {  //main function body
-    int dividend, divisor, quotient, remainder;  //variable declaration of integer data type
-    printf("Enter dividend: ");  //print Enter divident:
-    scanf("%d", &dividend);  //takes divident from user
-    printf("Enter divisor: ");  //print Enter divisor:
-    scanf("%d", &divisor);  //takes divisor from user
-
-    quotient = dividend / divisor;  // Computes quotient
-
-    remainder = dividend % divisor; //Computes remainder
-
-    printf("Quotient = %d\n", quotient);  //print Quotient
-    printf("Remainder = %d", remainder);  //print Remainder
-    return 0;
->>>>>>> d6eca3272e957df3637bfdd0277ca634454906bb
 }
