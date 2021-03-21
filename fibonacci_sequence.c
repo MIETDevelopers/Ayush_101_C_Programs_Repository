88b44a922f3fb38e0215febdfc46ea1a0d1ac9c0
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-03-22 00:15:50 +0530

    Update fibonacci_sequence.c

--- fibonacci_sequence.c
+++ fibonacci_sequence.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Author: Ayush Bhat
 // Creation Date: 18/3/21
 // Purpose Of The Program: to print fibonacci sequence using for loop
@@ -34,40 +33,3 @@ int main ()  //main function body
   return 0;
  
 }
-=======
-// Author: Ayush Bhat
-// Creation Date: 18/3/21
-// Purpose Of The Program: to print fibonacci sequence using for loop
-#include<stdio.h>  // preprocessor directive to include "stdio.h" header file having standard input and output functions
- 
-int main ()  //main function body
-{
-
-  int first = 0, second = 1, third, i, n; //varible declaration and intialazation(first, second)
- 
-  printf ("Enter the length of the fibonacci series \n");  //print Enter the length of fibonacci series
- 
-  scanf ("%d", &n);  //takes input from user
- 
-   printf("  The Fibonacci Series is:");  //print The Fibonacci Series
-    printf("  \n %d \n %d",first,second);  //print first and second number of series
- 
- //starting of for loop from 2 because 0 and 1 already printed
-  for (i = 2; i <= n; i++)  
- 
-    {
- 
-      third = first + second;
- 
-      printf ("\n %d ", third);
- 
-      first = second;
- 
-      second = third;
- 
-    }
- 
-  return 0;
- 
-}
->>>>>>> d6eca3272e957df3637bfdd0277ca634454906bb
