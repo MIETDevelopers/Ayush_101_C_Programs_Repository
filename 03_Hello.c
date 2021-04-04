eca19a27b989a2ac6c6901f679c3727bab0ab91d
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-04-04 04:32:15 -0700

    hello_world

---
+++ 03_Hello.c
@@ -0,0 +1,8 @@
+// Author: Ayush Bhat
+// Creation Date: 16/3/21
+// Purpose Of The Program: To print "Hello World"
+#include<stdio.h> // preprocessor directive to include "stdio.h" header file having standard input and output functions
+int main() { // main function body
+	printf("Hello World"); //printing the hello world
+	return 0;
+}
