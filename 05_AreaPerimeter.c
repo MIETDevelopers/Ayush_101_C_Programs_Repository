83e461e99fd9ae700a7b119a6e918447c78cf964
AYUSH BHAT <76398968+AYUSH-BHAT1@users.noreply.github.com>
2021-04-04 04:36:34 -0700

    Area_Perimeter

---
+++ 05_AreaPerimeter.c
@@ -0,0 +1,21 @@
+// Author: Ayush Bhat
+// Creation Date: 16/3/21
+// Purpose Of The Program: to find area and perimeter of circle, square and rectangle
+#include<stdio.h>  // preprocessor directive to include "stdio.h" header file having standard input and output functions
+int main(){  //main funnction body
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;  //variable declaration of float data type
+	
+	//variable initialization
+	radiusofCircle=5;
+	sideofSquare=4;
+	lengthofRectangle=6;
+	breadthofRectangle=3;
+	
+	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));   //print Area of Cricle
+	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));  //print Perimeter of circle
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));  //print Area of Square
+	printf("\nPerimeter of Square: %f",(4*sideOfSquare));  //print Perimeter of Square
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));  //print Area of Rectangle
+	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));  //print Perimeter of Rectangle
+	return 0;
+}
